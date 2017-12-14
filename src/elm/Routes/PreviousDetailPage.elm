62ec9b1ff7c8e5c548184eb05b00c1bbaef24626
astroash <astroaisha@gmail.com>
2017-12-14 18:06:15 +0000

    :lipstick: Style for previous detail page
    21 #21

--- src/elm/Routes/PreviousDetailPage.elm
+++ src/elm/Routes/PreviousDetailPage.elm
@@ -13,8 +13,8 @@ previousDetailPage model =
             case model.liveInteraction.notes.text of
                 intText ->
                     div
-                        [ class "flex v-mid bb bw2 b--black-10" ]
-                        [ p [ class "blue center f5" ] [ text intText ]
+                        [ class "flex v-mid" ]
+                        [ p [ class "blue center f5 ma0 pb3" ] [ text intText ]
                         ]
 
         interactionAudioHtml =
@@ -29,31 +29,48 @@ previousDetailPage model =
                 ]
             , div [ class "w-100 center ma0 pa0" ]
                 [ div [ class "inline-flex flex-wrap" ]
-                    [ button [ onClick <| PlayAudio True ] [ img [ class "ma3 br-100", src "./assets/svg_icons/hear_rec.svg" ] [ interactionAudioHtml ] ]
+                    [ button [ onClick <| PlayAudio True, class "button-transparent" ] [ img [ class "ma3 br-100", src "./assets/svg_icons/hear_rec.svg" ] [ interactionAudioHtml ] ]
                     , img [ class "ma3", src "./assets/svg_icons/man.svg" ] []
-                    , button [] [ img [ class "ma3 br-100", src "./assets/svg_icons/cal.svg" ] [] ]
+                    , button [ class "button-transparent" ] [ img [ class "ma3 br-100", src "./assets/svg_icons/cal.svg" ] [] ]
                     ]
                 ]
             , interactionAudioHtml
-            , div [ class "bb w-100" ]
+            , div [ class " w-100" ]
                 [ div [ class "bg-blue white pa0 ma0" ]
-                    [ h3 [ class "center pt2 underline" ] [ text "Date and time" ]
-                    , p [ class "ph2 pv0 f5 pa0" ] [ text "Met on Aug 3rd, 2017 at 6pm" ]
-                    , p [ class "ph2 pv0 f5 pa0" ] [ text "at New Town Hall, GL8 9JDF" ]
+                    [ h3 [ class "center pt3 underline" ] [ text "Date and time" ]
+                    , p [ class "f5" ] [ text "Met on Aug 3rd, 2017 at 6pm" ]
+                    , p [ class "f5 ma0 pb3" ] [ text "at New Town Hall, GL8 9JDF" ]
                     ]
                 , div [ class "bg-moon-gray blue ma0" ]
-                    [ h3 [ class "bold center pt3" ] [ text "Notes" ]
+                    [ h3 [ class "bold center pt3 mt0" ] [ text "Notes" ]
                     , interactionTextHtml
                     ]
-                , div [ class "bg-light-gray blue" ]
-                    [ h3 [] [ text "Contact" ]
-                    , p [] [ text "Email" ]
-                    , p [] [ text "Phone" ]
+                , div [ class "bg-light-gray blue ma0" ]
+                    [ h3 [ class "mt0 pt3" ] [ text "Contact" ]
+                    , p [] [ text <| "Email: " ++ model.liveInteraction.email ]
+                    , p [ class "pb3 ma0" ] [ text <| "Phone: " ++ model.liveInteraction.phone ]
                     ]
-                , div [ class "bg-white blue" ]
-                    [ h3 [] [ text "Actions" ]
+                , div [ class "bg-near-white blue ma0" ] <|
+                    [ h3 [ class "mt0 pt3" ] [ text "Actions" ]
                     , p [] [ text "Shared" ]
-                    , p [] [ text "Remainders" ]
+                    , p [ class "pb3 ma0" ] [ text "Reminders" ]
                     ]
                 ]
+            , div [ class "bg-white blue ma0" ] <|
+                [ h3 [ class "mt0 pt3" ] [ text "Recommendations" ]
+                ]
+                    ++ List.map recommendItem model.liveInteraction.recommendations
             ]
+
+
+recommendItem : Recommendation -> Html Msg
+recommendItem rec =
+    div []
+        [ p [] [ text <| "Shared with " ++ rec.name ++ " from " ++ rec.organisation ]
+        ]
+
+
+
+-- recommendContent :List Recommendation -> List Html Msg
+-- recommendContent recommendationList =
+--     List.Map  recommendation
