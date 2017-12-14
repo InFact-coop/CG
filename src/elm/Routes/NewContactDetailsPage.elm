ef6535d5baaa5657a9d29b63e5a92ad796ca718a
astroash <astroaisha@gmail.com>
2017-12-14 11:20:00 +0000

    :lipstick: Finish css on new contact page
     #1

--- src/elm/Routes/NewContactDetailsPage.elm
+++ src/elm/Routes/NewContactDetailsPage.elm
@@ -16,9 +16,11 @@ newContactDetailsPage model =
         , formItem "assets/svg_icons/phone.svg" "Phone" model.currentInteraction.phone SetContactPhone
         , formItem "assets/svg_icons/company.svg" "Organisation" model.currentInteraction.organisation SetContactOrganisation
         , div [ class "w-80 center" ]
-            [ p [ class "light-blue" ] [ text "Are you a member of Create Glost already?" ]
+            [ p [ class "blue" ] [ text "Are you a member of Create Glost already?" ]
             , buttonItem model.currentInteraction.currentMember CurrentMemberYes "Yeah"
             , buttonItem model.currentInteraction.currentMember CurrentMemberNo "No, but I would love to sign up!"
+            , p [ class "blue f4 tc w-80 center" ] [ text "Ok, save and continue to record a memo" ]
+            , a [ href "#newNotes" ] [ img [ src "assets/svg_icons/go.svg", class "center db mb3" ] [] ]
             ]
         ]
 
@@ -39,9 +41,9 @@ formItem imgSrc fieldName val msg =
 buttonItem : CurrentMemberOptions -> CurrentMemberOptions -> String -> Html Msg
 buttonItem currentMemberUTFromModel buttonClicked textValue =
     div []
-        [ button [ class "flex items-center", onClick (SetCurrentContact buttonClicked) ]
+        [ button [ class "flex items-center bn bg-white", onClick (SetCurrentContact buttonClicked) ]
             [ div [ class "h2 w2 ba bw1 b--light-blue dib br1", classList [ ( "bg-light-blue", checkboxMember buttonClicked currentMemberUTFromModel ) ] ] []
-            , p [ class "light-blue f4 dib ma0" ] [ text textValue ]
+            , p [ class "light-blue ml3 f5 dib ma0" ] [ text textValue ]
             ]
         ]
 
