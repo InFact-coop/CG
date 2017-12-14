8d16a91cf427b793019363e1396bc52452bfebe2
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-12-14 14:33:46 +0000

    added tags to the previousvisitspage

--- src/elm/Routes/PreviousOverviewPage.elm
+++ src/elm/Routes/PreviousOverviewPage.elm
@@ -4,14 +4,15 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Types exposing (..)
+import Components.TitleBar exposing (..)
 
 
 previousOverviewPage : Model -> Html Msg
 previousOverviewPage model =
     div [ class "w-60-ns center" ]
-        [ h2 [ class "blue center f3 db tc" ] [ text "CG Interactions" ]
-        , p [ class "blue center db tc" ] [ text "search by name, tags or organisation" ]
-        , input [ class "center db b-input pa2 ma2", onInput SetSearchInput, value model.searchInput, placeholder "search" ] []
+        [ titleBar "CG Interactions"
+        , p [ class "blue center db tc" ] [ text "Search by name, tags or organisation" ]
+        , input [ class "center db b-input pa2 ma2 w8", onInput SetSearchInput, value model.searchInput, placeholder "search" ] []
         , section
             [ class "center absolute imageInteractionItem" ]
             (interactionContent
@@ -25,14 +26,15 @@ interactionItem interaction =
     button [ class "w-100 interactionItem", onClick <| SelectInteractionItem interaction ]
         [ div [ class "" ]
             [ li [ class "db  bw2 b--blue-10" ]
-                [ h1 [ class " b blue" ] [ text interaction.organisation ]
-                , p [ class " b blue " ] [ text interaction.name ]
-                , div [ class "b blue " ] [ text interaction.notes.text ]
-                , div [ class "ma2" ]
-                    [ img [ src "./assets/svg_icons/email.svg", class "" ] []
-                    , img [ src "./assets/svg_icons/phone.svg", class "" ] []
-                    , img [ src "./assets/svg_icons/profile.svg", class "" ] []
-                    , img [ src "./assets/svg_icons/share.svg", class "" ] []
+                [ div [ class "bg-blue w3 h1 br3" ] [ p [ class "white" ] [ text interaction.tags ] ]
+                , h1 [ class "f4 b blue f3" ] [ text interaction.organisation ]
+                , p [ class "ma1 b blue f4" ] [ text interaction.name ]
+                , div [ class "b blue" ] [ text interaction.notes.text ]
+                , div [ class "ma2 flex justify-between pa2" ]
+                    [ img [ src "./assets/svg_icons/email.svg", class "ma2" ] []
+                    , img [ src "./assets/svg_icons/phone.svg", class "ma2" ] []
+                    , img [ src "./assets/svg_icons/profile.svg", class "ma2" ] []
+                    , img [ src "./assets/svg_icons/share.svg", class "ma2" ] []
                     ]
                 ]
             ]
